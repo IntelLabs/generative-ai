96455afd50138c3450078e9b2cfbffe66c721eda
Jascha Achterberg <51077955+8erberg@users.noreply.github.com>
2023-01-10 12:51:02 +0000

    Extended comment for automation setup

--- Dashboard_automation_setup.py
+++ Dashboard_automation_setup.py
@@ -1,6 +1,6 @@
 from pages.Functions.Assessment_functions import CLIP_single_object_classifier, CLIP_multi_object_recognition_DSwrapper, CLIP_object_negation, DETR_multi_object_counting_DSwrapper
 
-# Create dictionary to hold functions
+# Create dictionary to hold functions used for automated assessment
 fun_dict = {
     'Multiple object types':CLIP_multi_object_recognition_DSwrapper, 
     'Single object':CLIP_single_object_classifier,
