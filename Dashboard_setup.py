d4251d608c11b386ef2ceaa16a9dbf509341a18b
Jascha Achterberg <51077955+8erberg@users.noreply.github.com>
2023-01-10 12:53:31 +0000

    Extended comments for user specific setup

--- Dashboard_setup.py
+++ Dashboard_setup.py
@@ -1,4 +1,7 @@
 import pandas as pd 
 
+# List of tasks which are automated in current version - note that each of these needs a corresponding evaluation function in Dashboard_automation_setup.py
 automated_task_list = ['Multiple object types', 'Single object','Negation','Numbers (multiple objects)','Simple arithmetic']
+
+# Import the list of prompts used in current version
 prompt_dir = pd.read_csv('Data/Prompt_dir_230104.csv')
