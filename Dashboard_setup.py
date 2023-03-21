0062594560c96f49ee7b37c555d313e56a093105
8erberg <jascha.achterberg@icloud.com>
2023-03-21 14:48:53 +0000

    added new set of data used for annotation

--- Dashboard_setup.py
+++ Dashboard_setup.py
@@ -3,8 +3,8 @@ import pandas as pd
 
 # Dashboard version variables
 code_version = 'v0.0.5'
-prompt_dir_version = '230208'
-compatible_versions = ['0.0.5_pd230208', 'None']
+prompt_dir_version = '230228'
+compatible_versions = ['0.0.5_pd230228', 'None']
 dashboard_version_code = code_version+'_pd'+prompt_dir_version
 
 # List of tasks which are automated in current version - note that each of these needs a corresponding evaluation function in Dashboard_automation_setup.py
