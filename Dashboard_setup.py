9c37aa7eec432e34e5e2185fdfbdd7cff310a483
8erberg <jascha.achterberg@icloud.com>
2023-03-24 18:19:20 +0000

    updated dashboard setup files to allow for automated annotation of counting and multiple objects

--- Dashboard_setup.py
+++ Dashboard_setup.py
@@ -4,11 +4,12 @@ import pandas as pd
 # Dashboard version variables
 code_version = 'v0.0.5'
 prompt_dir_version = '230228'
-compatible_versions = ['0.0.5_pd230228', 'None']
+compatible_versions = ['0.0.5_pd230208','0.0.5_pd230228', 'None']
 dashboard_version_code = code_version+'_pd'+prompt_dir_version
 
 # List of tasks which are automated in current version - note that each of these needs a corresponding evaluation function in Dashboard_automation_setup.py
-automated_task_list = ['Multiple object types', 'Single object','Negation','Numbers (multiple objects)','Simple arithmetic','Conditional generation']
+automated_task_list = ['Multiple objects', 'Single object','Negation','Counting','Simple arithmetic','Conditional generation']
+
 
 # Import the list of prompts used in current version
 prompt_dir = pd.read_csv('data/Prompt_dir_{0}.csv'.format(prompt_dir_version))
